868b274da2f8985c6237405549afd086ad0ce662
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-09-16 11:06:18 +0100

    wrote failing test for two words

--- wordwrap/spec/wordwrap_spec.rb
+++ wordwrap/spec/wordwrap_spec.rb
@@ -12,12 +12,20 @@ describe "wordwrap" do
       "coffee" => "coffee"
     } 
   
-    same_length_string.each do |string, output|
-      it "doesn't wrap if line length is same as word length" do
-        expect(WordWrap.new.wrap(string, string.length)).to eq(output)
-      end
+  same_length_string.each do |string, output|
+    it "doesn't wrap if line length is same as word length" do
+      expect(WordWrap.new.wrap(string, string.length)).to eq(output)
     end
-    
+  end
+  
+  two_words_string = {
+    "hello world" => "hello\nworld"
+  } 
 
+  two_words_string.each do |string, output|
+    it "wraps after whitespace" do
+      expect(WordWrap.new.wrap(string, 5)).to eq(output)
+    end
+  end 
 
 end
