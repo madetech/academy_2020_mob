c22cfcf5a855d113c769d59670b6f07accf2c1fb
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-09-16 13:04:37 +0100

    Three different wrap-after-space test implementations side by side.

--- wordwrap/spec/wordwrap_spec.rb
+++ wordwrap/spec/wordwrap_spec.rb
@@ -17,15 +17,38 @@ describe "wordwrap" do
       expect(WordWrap.new.wrap(string, string.length)).to eq(output)
     end
   end
+
+  ## NB: The following test is implemented three times to compare different methods.
   
-  two_words_string = {
+  two_words_string_01 = {
+    "hello\nworld" => ["hello world", 5],
+    "hello\nhello" => ["hello hello", 5],
+    "coffee\ncoffee" => ["coffee coffee", 6]
+  } 
+  two_words_string_01.each do |output, input|
+    it "wraps after space (v1)" do
+      expect(WordWrap.new.wrap(input[0], input[1])).to eq(output)
+    end
+  end 
+
+  two_words_string_02 = [
+    ["hello world", 5, "hello\nworld"],
+    ["hello hello", 5, "hello\nhello"],
+    ["coffee coffee", 6, "coffee\ncoffee"]
+  ] 
+  two_words_string_02.each do |string_to_wrap, line_length, expected_output|
+    it "wraps after space (v2)" do
+      expect(WordWrap.new.wrap(string_to_wrap, line_length)).to eq(expected_output)
+    end
+  end 
+  
+  two_words_string_03 = {
     "hello world" => {:line_length => 5, :expected_output => "hello\nworld"},
     "hello hello" => {:line_length => 5, :expected_output => "hello\nhello"},
     "coffee coffee" => {:line_length => 6, :expected_output => "coffee\ncoffee"}
   } 
-
-  two_words_string.each do |string_to_wrap, values|
-    it "wraps after space" do
+  two_words_string_03.each do |string_to_wrap, values|
+    it "wraps after space (v3)" do
       expect(WordWrap.new.wrap(string_to_wrap, values[:line_length])).to eq(values[:expected_output])
     end
   end 
