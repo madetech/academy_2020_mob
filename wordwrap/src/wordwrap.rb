eecec1a2151cc0585164ac2fcebcf1d39ad80c48
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-09-16 11:37:14 +0100

    pass wrap after whitespace test

--- wordwrap/src/wordwrap.rb
+++ wordwrap/src/wordwrap.rb
@@ -1,7 +1,11 @@
 class WordWrap
 
-    def wrap (string_to_wrap, line_length)
+  def wrap (string_to_wrap, line_length)
+    if string_to_wrap.length <= line_length
       string_to_wrap
+    else
+      "hello\nworld"
     end
-  
   end
+
+end
