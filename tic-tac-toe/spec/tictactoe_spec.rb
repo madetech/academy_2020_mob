daa6b8180b0a69cf032702dd2b50ddd540f149e5
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-09-30 14:46:58 +0100

    Added a context

--- tic-tac-toe/spec/tictactoe_spec.rb
+++ tic-tac-toe/spec/tictactoe_spec.rb
@@ -13,13 +13,15 @@ RSpec.describe 'The HelloWorld App' do
       MyApp
     end
   
-    it "starts with an empty 3x3 tic-tac-toe grid" do
-      get '/tictactoe'
-      expect(last_response).to be_ok
-      for row in 0..2 
-        for col in 0..2 
-          expect(last_response.body).to have_tag('input', :with => { :class => "row#{row+1} col#{col+1}" })
-        end
-      end 
+    context "simple display" do
+      it "starts with an empty 3x3 tic-tac-toe grid" do
+        get '/tictactoe'
+        expect(last_response).to be_ok
+        for row in 0..2 
+          for col in 0..2 
+            expect(last_response.body).to have_tag('input', :with => { :class => "row#{row+1} col#{col+1}" })
+          end
+        end 
+      end
     end
 end
