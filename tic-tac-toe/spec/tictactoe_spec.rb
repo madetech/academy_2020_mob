8a97bd20e95b485cbb77c0b6dac4a707212a5142
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-09-30 15:02:27 +0100

    Now allows user to submit data

--- tic-tac-toe/spec/tictactoe_spec.rb
+++ tic-tac-toe/spec/tictactoe_spec.rb
@@ -27,4 +27,30 @@ RSpec.describe 'The HelloWorld App' do
         end 
       end
     end
+    
+    context "updated display after user input" do
+      # Arrange
+      grid_cells_with_css = {
+        :row0_col0_in => {:css => 'input.row0.col0', :input => "A"},
+        :row0_col1_in => {:css => 'input.row0.col1', :input => "B"},
+        :row0_col2_in => {:css => 'input.row0.col2', :input => "C"},
+        :row1_col0_in => {:css => 'input.row1.col0', :input => "D"},
+        :row1_col1_in => {:css => 'input.row1.col1', :input => "E"},
+        :row1_col2_in => {:css => 'input.row1.col2', :input => "F"},
+        :row2_col0_in => {:css => 'input.row2.col0', :input => "G"},
+        :row2_col1_in => {:css => 'input.row2.col1', :input => "H"},
+        :row2_col2_in => {:css => 'input.row2.col2', :input => "I"}
+      }
+
+      grid_cells_with_css.each do |control, values|
+        it "remembers when user makes a mark in a grid cell: '#{control}, #{values[:input]}'" do 
+          # Act
+          post "/tictactoe", control => values[:input] 
+
+          # Assert
+          expect(last_response).to be_ok
+          expect(last_response.body).to have_tag(values[:css], :with => { :value => values[:input] })
+        end
+      end
+    end
 end
--- tic-tac-toe/tictactoe.rb
+++ tic-tac-toe/tictactoe.rb
@@ -5,6 +5,17 @@ class MyApp < Sinatra::Base
     enable :sessions
 
     get '/tictactoe' do
+        @cells = Array.new(3){ Array.new(3) { "" } }
+        erb :tictactoe
+    end
+
+    post '/tictactoe' do
+        @cells = Array.new(3){ Array.new(3) { "" } }
+        for row in 0..2 
+            for col in 0..2 
+                @cells[row][col] = params["row#{row}_col#{col}_in"]
+            end
+        end
         erb :tictactoe
     end
 
