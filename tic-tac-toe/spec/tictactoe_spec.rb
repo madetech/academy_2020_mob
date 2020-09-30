e2892c23f53b0f946bbd9aa74c39f4bfb5681cb4
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-09-30 14:49:05 +0100

    Use 0-based indexing for css classes.

--- tic-tac-toe/spec/tictactoe_spec.rb
+++ tic-tac-toe/spec/tictactoe_spec.rb
@@ -15,11 +15,14 @@ RSpec.describe 'The HelloWorld App' do
   
     context "simple display" do
       it "starts with an empty 3x3 tic-tac-toe grid" do
+        # Act
         get '/tictactoe'
+        
+        # Assert
         expect(last_response).to be_ok
         for row in 0..2 
           for col in 0..2 
-            expect(last_response.body).to have_tag('input', :with => { :class => "row#{row+1} col#{col+1}" })
+            expect(last_response.body).to have_tag('input', :with => { :class => "row#{row} col#{col}" })
           end
         end 
       end
