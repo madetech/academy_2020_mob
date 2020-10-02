3cca4a9cb0b185fa0689328d5366e9dd5619ad72
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-10-02 10:03:41 +0100

    First acceptance test

---
+++ mars-rover/spec/acceptance_tests_spec.rb
@@ -0,0 +1,6 @@
+Given "the user hasn't started the program yet"
+When "the user starts the program"
+Then "an empty 5*5 grid appears"
+
+
+
--- mars-rover/spec/marsrover_spec.rb
+++ mars-rover/spec/marsrover_spec.rb
@@ -1,4 +1,19 @@
 require_relative '../lib/marsrover'
 
 describe "marsrover" do
+    context ""
 end
+
+
+
+# describe "marsrover" do
+#     context "the rover receives an empty array" do
+#         it "does not alter its position" do
+#             rover = MarsRover.new([0, 0], "N")
+#             rover.move([])
+#             expect(rover.position).to eq([0, 0])
+#             expect(rover.direction).to eq("N")
+#         end
+#     end
+# end
+
