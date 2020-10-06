857d6a72ae71565a6d3bb6a271210a370e4f638a
joannafawl <joanna.fawl@madetech.com>
2020-10-06 10:27:38 +0100

    Added a mars rover object to the grid class

--- mars-rover/lib/grid.rb
+++ mars-rover/lib/grid.rb
@@ -1,8 +1,9 @@
 class Grid
-    attr_accessor :display
+    attr_accessor :display, :mars_rover
 
-    def initialize
+    def initialize(mars_rover)
         @display = "5*5 grid"
+        @mars_rover = mars_rover
     end
 
     def show_display
--- mars-rover/spec/grid_spec.rb
+++ mars-rover/spec/grid_spec.rb
@@ -10,6 +10,16 @@ describe "grid" do
             #Assert
             expect(grid.display).to eq(empty_grid)
         end
+
+        it "initializes a mars rover" do
+            #Arrange
+            mars_rover = "mars rover"
+            #Act
+            grid = Grid.new(mars_rover)
+            #Assert
+            expect(grid.mars_rover).to eq(mars_rover)
+        end
+
         it "displays an empty 5x5 grid" do
             #Arrange
             grid = Grid.new
