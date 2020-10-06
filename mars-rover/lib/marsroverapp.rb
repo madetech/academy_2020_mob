34c035fdb95cd42c3775db2f9e3f73b86fc6859b
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-10-06 22:07:35 +0100

    Refactor: Extract method

--- mars-rover/lib/marsroverapp.rb
+++ mars-rover/lib/marsroverapp.rb
@@ -5,6 +5,12 @@ class MarsRoverApp
     def start(presenter, grid, mars_rover)
         presenter.show_display(grid)
         user_input = presenter.get_input(REQUEST_FOR_INPUT)
+        move_rover(user_input)
+    end
+
+    private
+
+    def move_rover(new_position)
         mars_rover.move(user_input)
         grid.update(mars_rover)
         presenter.show_display(grid)
