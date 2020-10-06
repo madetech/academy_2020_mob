7e45619571888490c9f3639f26aee7b47ce625b0
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-10-06 22:05:27 +0100

    New Mars Rover class with the ability to move.

--- mars-rover/lib/marsrover.rb
+++ mars-rover/lib/marsrover.rb
@@ -1,2 +1,13 @@
 class MarsRover
+    attr_accessor :x, :y, :direction
+
+    def initialize(x, y, direction)
+        @x = x
+        @y = y
+        @direction = direction
+    end
+
+    def move(x, y, direction)
+        initialize(x, y, direction)
+    end
 end
--- mars-rover/spec/marsrover_spec.rb
+++ mars-rover/spec/marsrover_spec.rb
@@ -1,20 +1,41 @@
 require_relative '../lib/marsrover'
 
-describe "marsrover" do
-    context ""
+describe "MarsRover" do
+    context "#initialize" do
+        it "sets position and direction according to data passed in" do
+            # Arrange 
+            EXPECTED_X = 4
+            EXPECTED_Y = 5
+            EXPECTED_DIRECTION = "E"
+            
+            # Act
+            mars_rover = MarsRover.new(EXPECTED_X, EXPECTED_Y, EXPECTED_DIRECTION)
+
+            # Assert
+            expect(mars_rover.x).to eq(EXPECTED_X)
+            expect(mars_rover.y).to eq(EXPECTED_Y)
+            expect(mars_rover.direction).to eq(EXPECTED_DIRECTION)
+        end
+    end
+    
+    context "#move" do
+        it "changes position and direction according to data passed in" do
+            # Arrange 
+            EXPECTED_X = 4
+            EXPECTED_Y = 5
+            EXPECTED_DIRECTION = "E"
+            INITIAL_DIRECTION = "W"
+            mars_rover = MarsRover.new(EXPECTED_X-1, EXPECTED_Y-1, INITIAL_DIRECTION)
+            
+            # Act
+            mars_rover.move(EXPECTED_X, EXPECTED_Y, EXPECTED_DIRECTION)
+
+            # Assert
+            expect(mars_rover.x).to eq(EXPECTED_X)
+            expect(mars_rover.y).to eq(EXPECTED_Y)
+            expect(mars_rover.direction).to eq(EXPECTED_DIRECTION)
+        end
+    end
 end
-
-
-
-# describe "marsrover" do
-#     context "the rover receives an empty array" do
-#         it "does not alter its position" do
-#             rover = MarsRover.new([0, 0], "N")
-#             rover.move([])
-#             expect(rover.position).to eq([0, 0])
-#             expect(rover.direction).to eq("N")
-#         end
-#     end
-# end
 
 
