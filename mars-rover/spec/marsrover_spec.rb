44599bff80467ff23643b3773960037ba5ee95dc
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-10-07 18:13:09 +0100

    Add test cases for moving backwards.

--- mars-rover/spec/marsrover_spec.rb
+++ mars-rover/spec/marsrover_spec.rb
@@ -20,83 +20,47 @@ describe "MarsRover" do
     end
 
     context "#move" do
-        it "will not change direction given input 'f'" do
-            # Arrange 
-            START_X = 0
-            START_Y = 0
-            DIRECTION = MarsRover.NORTH
-            mars_rover = MarsRover.new
-            mars_rover.start(START_X, START_Y, DIRECTION)
-            
-            # Act
-            mars_rover.move(MarsRover.FORWARD)
+        simple_directions = [[MarsRover.FORWARD, MarsRover.NORTH], 
+                            [MarsRover.BACKWARD, MarsRover.SOUTH]]
 
-            # Assert
-            expect(mars_rover.direction).to eq(DIRECTION)
+        directions.each do |movement, expected_direction|
+            it "will not change direction given input '#{movement}'" do
+                # Arrange 
+                START_X = 0
+                START_Y = 0
+                mars_rover = MarsRover.new
+                mars_rover.start(START_X, START_Y, expected_direction)
+                
+                # Act
+                mars_rover.move(movement)
+
+                # Assert
+                expect(mars_rover.direction).to eq(expected_direction)
+            end
         end
 
-        it "will move up one square given input 'f' and direction N" do
-            # Arrange 
-            START_X = 0
-            START_Y = 0
-            DIRECTION = MarsRover.NORTH
-            mars_rover = MarsRover.new
-            mars_rover.start(START_X, START_Y, DIRECTION)
-            
-            # Act
-            mars_rover.move(MarsRover.FORWARD)
+        all_directions = [[MarsRover.FORWARD, MarsRover.NORTH, {:x=>0,:y=>0}, {:x=>0,:y=>1}], 
+                          [MarsRover.FORWARD, MarsRover.EAST, {:x=>0,:y=>0}, {:x=>1,:y=>0}], 
+                          [MarsRover.FORWARD, MarsRover.SOUTH, {:x=>4,:y=>4}, {:x=>4,:y=>3}], 
+                          [MarsRover.FORWARD, MarsRover.WEST, {:x=>4,:y=>4}, {:x=>3,:y=>4}],
+                          [MarsRover.BACKWARD, MarsRover.NORTH, {:x=>4,:y=>4}, {:x=>4,:y=>3}], 
+                          [MarsRover.BACKWARD, MarsRover.EAST, {:x=>4,:y=>4}, {:x=>3,:y=>4}], 
+                          [MarsRover.BACKWARD, MarsRover.SOUTH, {:x=>0,:y=>0}, {:x=>0,:y=>1}], 
+                          [MarsRover.BACKWARD, MarsRover.WEST, {:x=>0,:y=>0}, {:x=>1,:y=>0}]]
 
-            # Assert
-            expect(mars_rover.x).to eq(START_X)
-            expect(mars_rover.y).to eq(START_Y + 1)
-        end
+        all_directions.each do |movement, direction, start_pos, expected_pos|
+            it "will move up one square given input '#{movement}' and direction '#{direction}'" do
+                # Arrange 
+                mars_rover = MarsRover.new
+                mars_rover.start(start_pos[:x], start_pos[:y], direction)
+                
+                # Act
+                mars_rover.move(movement)
 
-        it "will move right one square given input 'f' and direction 'E'" do
-            # Arrange 
-            START_X = 0
-            START_Y = 0
-            DIRECTION = MarsRover.EAST
-            mars_rover = MarsRover.new
-            mars_rover.start(START_X, START_Y, DIRECTION)
-            
-            # Act
-            mars_rover.move(MarsRover.FORWARD)
-
-            # Assert
-            expect(mars_rover.x).to eq(START_X + 1)
-            expect(mars_rover.y).to eq(START_Y)
-        end
-
-        it "will move down one square given input 'f' and direction 'S'" do
-            # Arrange 
-            START_X = 0
-            START_Y = 4
-            DIRECTION = MarsRover.SOUTH
-            mars_rover = MarsRover.new
-            mars_rover.start(START_X, START_Y, DIRECTION)
-            
-            # Act
-            mars_rover.move(MarsRover.FORWARD)
-
-            # Assert
-            expect(mars_rover.x).to eq(START_X)
-            expect(mars_rover.y).to eq(START_Y - 1)
-        end
-
-        it "will move left one square given input 'f' and direction 'W'" do
-            # Arrange 
-            START_X = 4
-            START_Y = 0
-            DIRECTION = MarsRover.WEST
-            mars_rover = MarsRover.new
-            mars_rover.start(START_X, START_Y, DIRECTION)
-            
-            # Act
-            mars_rover.move(MarsRover.FORWARD)
-
-            # Assert
-            expect(mars_rover.x).to eq(START_X - 1)
-            expect(mars_rover.y).to eq(START_Y)
+                # Assert
+                expect(mars_rover.x).to eq(expected_pos[:x])
+                expect(mars_rover.y).to eq(expected_pos[:y])
+            end
         end
     end
 end
