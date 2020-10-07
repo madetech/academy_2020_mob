09cc80b104328fc5dcb147208c1095972481c3ec
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-10-07 11:54:00 +0100

    Add test example usingf test spies.

--- mars-rover/spec/marsroverapp_spec.rb
+++ mars-rover/spec/marsroverapp_spec.rb
@@ -58,4 +58,26 @@ describe MarsRoverApp do
             expect{marsroverapp.start(fake_presenter, grid_stub, mars_rover_stub)}.std_output.to have_as_last_output(GRID_WITH_NEW_ROVER)
         end
     end
+
+    context "when moving (using test spies)" do
+        it "uses the rover, the grid and the presenter to update the Rover's position" do
+            # Arrange
+            marsroverapp = MarsRoverApp.new
+            GRID_WITH_NEW_ROVER = "This is what we think a 5x5 grid with a North-facing Rover at 0,0 will look like."
+            GRID_WITH_ROVER = make_grid(5, 5, 0, 0, "N")
+            mars_rover_spy = MarsRover.spy
+            grid_spy = Grid.spy
+            presenter_spy = Presenter.spy(show_display(grid_spy) => puts GRID_WITH_NEW_ROVER)
+            EXPECTED_INPUT = "0, 0, N"
+            stub gets(EXPECTED_INPUT) 
+
+            # Act/Assert
+            expect{marsroverapp.start(presenter_spy, grid_spy, mars_rover_spy)}.std_output.to have_as_last_output(GRID_WITH_NEW_ROVER)
+            expect(mars_rover_spy.x).to be_called
+            expect(mars_rover_spy.y).to be_called
+            expect(mars_rover_spy.direction).to be_called
+            expect(grid_spy.update).to be_called with(mars_rover_spy)
+            expect(presenter_spy.show_display).to be_called with(grid_spy)
+        end
+    end
 end
